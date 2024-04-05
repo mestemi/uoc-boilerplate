6a5ac099ce0547fb347d5096936d64ec2d53cfb8
Macià Estela Miarnau <mestemi@uoc.edu>
2024-04-05 21:57:16 +0200

    prueba owl carousel

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -18,11 +18,6 @@
 $(document).ready(function(){
   console.log("aaa");
   $('.owl-carousel').owlCarousel({
-    items : 1,
-    loop  : true,
-    margin : 30,
-    nav    : true,
-    smartSpeed :900,
-    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+    items : 1
   });
 });
